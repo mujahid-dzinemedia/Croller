00f2bda413a7161aaf93f192ab70f096020fb321
mujahid-dzinemedia <106052326+mujahid-dzinemedia@users.noreply.github.com>
2022-10-08 13:08:44 +0500

    AndroidX

--- app/src/main/java/com/sdsmdg/harjot/crollerTest/MainActivity.java
+++ app/src/main/java/com/sdsmdg/harjot/crollerTest/MainActivity.java
@@ -1,8 +1,8 @@
 package com.sdsmdg.harjot.crollerTest;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.SwitchCompat;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import android.widget.CompoundButton;
 import android.widget.Toast;
 
